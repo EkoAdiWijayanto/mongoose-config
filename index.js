fc41a14a204df214995996e2ff7fda6a54b3934f
Eko Adi Wijayanto <eko.adi.wijayanto@gmail.com>
2017-02-07 09:36:33 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var config = require('config');
+
+
+mongoose.Promise = require('bluebird');
+mongoose.set('debug', config.server.database.debug);
+mongoose.connect(config.server.database.url);
